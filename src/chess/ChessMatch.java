d9b7f4580f316b8bb473b83c3d564001045b8a93
Alexandre Barth <alebarth2001@gmail.com>
2022-04-13 19:05:04 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -46,6 +46,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 	
 	//Para colocar peças usando a posição do Tabuleiro de Xandrez e não da matriz
